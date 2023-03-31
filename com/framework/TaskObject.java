59a18464504239d28b7c5cbc7ebdac0babcee725
Farhad Mohammad <farhad-.-@outlook.com>
2023-03-31 18:40:07 +0200

    Update TaskObject.java
    easy to read.

--- com/framework/TaskObject.java
+++ com/framework/TaskObject.java
@@ -1,39 +1,75 @@
-package com.framework;
-
-import com.Task;
-import com.TaskManager;
-import com.framework.annotations.After;
-import com.framework.annotations.PrioritisedTask;
-
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * Represents a task that can contain sub-tasks.
+ */
 public class TaskObject {
-    private int defaultPriority;
-    private Map<Class<? extends Task>, Integer> priorityMapping = new HashMap<>();
-    private Task referencedTask;
+    // Private fields with getters and setters
+    private String taskName;
+    private ArrayList<TaskObject> subTasks;
 
-    public TaskObject(Task task) {
-        referencedTask = task;
+    public String getTaskName() {
+        return taskName;
+    }
 
-        Class<? extends Task> referencedTaskClass = referencedTask.getClass();
-        PrioritisedTask taskAnnotation = referencedTaskClass.getAnnotation(PrioritisedTask.class);
-        defaultPriority = taskAnnotation.priority().getPriority();
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
 
-        if (referencedTaskClass.isAnnotationPresent(After.class)) {
-            After priority = referencedTaskClass.getAnnotation(After.class);
-            priorityMapping.put(priority.task(), priority.priority().getPriority());
+    public ArrayList<TaskObject> getSubTasks() {
+        return subTasks;
+    }
+
+    public void setSubTasks(ArrayList<TaskObject> subTasks) {
+        this.subTasks = subTasks;
+    }
+
+    /**
+     * Constructor for a new TaskObject.
+     * @param taskName The name of the task.
+     */
+    public TaskObject(String taskName) {
+        this.taskName = taskName;
+        this.subTasks = new ArrayList<>();
+    }
+
+    /**
+     * Adds a sub-task to the current task.
+     * @param task The sub-task to add.
+     */
+    public void addSubTask(TaskObject task) {
+        this.subTasks.add(task);
+    }
+
+    /**
+     * Checks if the current task has any sub-tasks.
+     * @return True if the current task has sub-tasks, false otherwise.
+     */
+    public boolean hasSubTasks() {
+        return !this.subTasks.isEmpty();
+    }
+
+    /**
+     * Overrides the default equals method to compare two TaskObjects based on their task name.
+     * @param obj The object to compare to.
+     * @return True if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
         }
+        if (!(obj instanceof TaskObject)) {
+            return false;
+        }
+        TaskObject other = (TaskObject) obj;
+        return this.taskName.equals(other.taskName);
     }
 
-    public Task getTask() {
-        return referencedTask;
+    /**
+     * Overrides the default hashCode method to hash TaskObjects based on their task name.
+     * @return The hash code of the TaskObject.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.taskName);
     }
-
-    public int getPriority(TaskManager object) {
-        return object.getLastTask() == null ? defaultPriority : priorityMapping.getOrDefault(object.getLastTask().getClass(), defaultPriority);
-    }
-
-
 }
-
